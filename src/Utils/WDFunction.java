f7755d17d161af8094aeb9fddcad0d9c370dba1b
Ashif927 <ashif.nusrat1206@gmail.com>
2017-05-28 12:41:50 -0400

    Package and Class Added

---
+++ src/Utils/WDFunction.java
@@ -0,0 +1,11 @@
+package Utils;
+
+public class WDFunction {
+	public void typeByXpath (String locators, String value){
+	};
+	public void typeByName (String locators, String value){
+	};
+	public void typeByID (String locators, String value){
+	};
+
+}
